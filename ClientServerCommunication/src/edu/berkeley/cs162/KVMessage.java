60709a9c29bc38f182e4406a8ca3a3200e6efb6a
Kelvin Chen <kelvin.c.chen@gmail.com>
2013-04-14 18:08:26 -0700

    these wounds they will not heaaaaaaaaaaaaaaaaaaal

--- ClientServerCommunication/src/edu/berkeley/cs162/KVMessage.java
+++ ClientServerCommunication/src/edu/berkeley/cs162/KVMessage.java
@@ -38,6 +38,7 @@ import java.net.Socket;
 /**
  * This is the object that is used to generate messages the XML based messages 
  * for communication between clients and servers. .../afsdfas
+ * crawling in my skin
  */
 public class KVMessage {
 	private String msgType = null;
