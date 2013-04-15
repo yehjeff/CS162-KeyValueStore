4a8cf688a3079c6d593bba66c17e4ac247a165f3
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-04-14 22:08:07 -0700

    Described some test cases in comments. Not coded yet.

--- ClientServerCommunication/src/edu/berkeley/cs162/TestKVCache.java
+++ ClientServerCommunication/src/edu/berkeley/cs162/TestKVCache.java
@@ -4,4 +4,34 @@ import junit.framework.TestCase;
 
 public class TestKVCache extends TestCase {
 
+	
+	public void testConstructor() {
+		//Just check that all entries are invalid after instantiation
+	}
+	
+	//SUPER TEST FUNCTION
+	public void testGetPutDel() {	//what about null arguments?
+		//WITH AN EMPTY CACHE, all calls to get(key) should return null for any key
+		
+		//WITH NONEMPTY CACHE
+		
+	}
+	
+	
+	
+	
+	public void testGetWriteLock() {
+		// idk lol
+	}
+	
+	public void testToXml() {
+		//Check the return value of toXML() with an empty cache
+		KVStore storeEmpty = new KVStore();
+				
+		//Check the return value of toXML() with a non-empty cache
+		KVStore storeNonEmpty = new KVStore();
+		
+		//Check the return value of toXML() with a full cache
+	}
+	
 }
--- ClientServerCommunication/src/edu/berkeley/cs162/TestKVStore.java
+++ ClientServerCommunication/src/edu/berkeley/cs162/TestKVStore.java
@@ -3,5 +3,56 @@ package edu.berkeley.cs162;
 import junit.framework.TestCase;
 
 public class TestKVStore extends TestCase {
+	
+	public void testToXML() {
+		//Check the return value of toXML() with an empty store
+		KVStore storeEmpty = new KVStore();
+		
+		//Check the return value of toXML() with a non-empty store
+		KVStore storeNonEmpty = new KVStore();
 
+	}
+	
+	public void testDumpToFile() {
+		//Check the file output of dumpToFile() with an empty store
+		
+		//Check the file output of dumpToFile() with a non-empty store
+		
+	}
+	
+	public void testRestoreFromFile() {
+		//USING toXML() 
+		//WITH INITIALLY EMPTY STORE
+		//Check store after call to restoreFromFile() with empty xml file
+		
+		//Check store after call to restoreFromFile() with proper xml file with no key-value pairs
+		
+		//Check store after call to restoreFromFile() with proper xml file with some key-value pairs 
+		
+		//Check store after call to restoreFromFile() with an invalid xml file
+		
+		//WITH INITIALLY NON-EMPTY STORE
+		//Check store after call to restoreFromFile() with empty xml file
+		//store should just be empty (?????) or exception thrown??
+		
+		//Check store after call to restoreFromFile() with proper xml file with no key-value pairs
+		//store should be empty
+		
+		//Check store after call to restoreFromFile() with proper xml file with some key-value pairs with keys that DONT match the ones in the store
+		//store should just have whats inside xml file
+		
+		
+		//Check store after call to restoreFromFile() with proper xml file with some key-value pairs with keys that DO match the ones in the store
+		//store should just have whats inside xml file
+		
+		
+		//Check store after call to restoreFromFile() with an invalid xml file
+		//store should just be empty (?????) or exception thrown??
+		
+
+
+
+	}
+	
+	
 }
