daaa0d67d9aba3fbde0884fafed528909ffc0af7
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-04-15 18:27:20 -0700

    Added tests and stuff

--- ClientServerCommunication/src/edu/berkeley/cs162/TestKVCache.java
+++ ClientServerCommunication/src/edu/berkeley/cs162/TestKVCache.java
@@ -7,31 +7,114 @@ public class TestKVCache extends TestCase {
 	
 	public void testConstructor() { 
 		//Just check that all entries are invalid after instantiation //HOW TO TEST IF EVERYTHING IS PRIVATE????
-		int numSets = 4;
-		int maxElemsPerSet = 4;
-		KVCache cache = new KVCache(numSets,maxElemsPerSet);
-		for (int i = 0; i < numSets; i ++){
-			for (int j = 0; j < maxElemsPerSet; j++){
-				
-				assertTrue(true);
-			}
-		}
+		
 
 	}
 	
 	//SUPER TEST FUNCTION
 	public void testGetPutDel() {	//what about null arguments?
-		//WITH AN EMPTY CACHE, all calls to get(key) should return null for any key
 		int numSets = 4;
 		int maxElemsPerSet = 4;
 		KVCache cache = new KVCache(numSets,maxElemsPerSet);
-		cache.put("key1", "value1");
+		
+		//all calls to get should return null when nothing is inside the cache
+		String getReturnValue;
+		getReturnValue = cache.get("key1");
+		assertTrue(getReturnValue == null);
+		getReturnValue = cache.get("key2");
+		assertTrue(getReturnValue == null);
+		getReturnValue = cache.get("key3");
+		assertTrue(getReturnValue == null);
 		System.out.println(cache.toXML());
-		//WITH NONEMPTY CACHE
+		
+		//using get to ensure put was successful
+		cache.put("key1", "value1");
+		getReturnValue = cache.get("key1");
+		assertTrue(getReturnValue != null);
+		assertTrue(getReturnValue.equals("value1"));
+		
+		//and get should still not work for other keys that were not put'd
+		getReturnValue = cache.get("key2");
+		assertTrue(getReturnValue == null);
+		
+		//trying multiple puts with different keys
+		cache.put("key2", "value2");
+		getReturnValue = cache.get("key1");
+		assertTrue(getReturnValue != null);
+		assertTrue(getReturnValue.equals("value1"));
+		getReturnValue = cache.get("key2");
+		assertTrue(getReturnValue != null);
+		assertTrue(getReturnValue.equals("value2"));
+		
+		//using put to overwrite a previous put
+		cache.put("key1","altvalue1");
+		getReturnValue = cache.get("key1");
+		assertTrue(getReturnValue != null);
+		assertTrue(getReturnValue.equals("altvalue1"));
+		
+		//now using delete to ensure a kv pair gets deleted
+		cache.del("key1");
+		getReturnValue = cache.get("key1");
+		assertTrue(getReturnValue == null);
+
+		
 		
 	}
 	
-	
+	public void testEvictionPolicy(){
+		//to ensure kv pairs are in the same set, have a single-set cache
+		int numSets = 1;
+		int maxElemsPerSet = 4;
+		KVCache cache = new KVCache(numSets, maxElemsPerSet);
+		String getReturn;
+		//fill up the set first
+		cache.put("key1", "value1");
+		cache.put("key2", "value2");
+		cache.put("key3", "value3");
+		cache.put("key4", "value4");
+		
+		//add a new kvpair, key5, then key1 should be replaced, since it was the first one to be added
+		cache.put("key5", "value5");
+		getReturn = cache.get("key1");
+		assertTrue(getReturn == null);
+		getReturn = cache.get("key2");
+		assertTrue(getReturn != null);
+		assertTrue(getReturn.equals("value2"));
+		getReturn = cache.get("key3");
+		assertTrue(getReturn != null);
+		assertTrue(getReturn.equals("value3"));
+		getReturn = cache.get("key4");
+		assertTrue(getReturn != null);
+		assertTrue(getReturn.equals("value4"));
+		getReturn = cache.get("key5");
+		assertTrue(getReturn != null);
+		assertTrue(getReturn.equals("value5"));
+		
+		//now key2 should be replaced
+		cache.put("key6","value6");
+		getReturn = cache.get("key1");
+		assertTrue(getReturn == null);
+		getReturn = cache.get("key2");
+		assertTrue(getReturn == null);
+		getReturn = cache.get("key3");
+		assertTrue(getReturn != null);
+		assertTrue(getReturn.equals("value3"));
+		getReturn = cache.get("key4");
+		assertTrue(getReturn != null);
+		assertTrue(getReturn.equals("value4"));
+		getReturn = cache.get("key5");
+		assertTrue(getReturn != null);
+		assertTrue(getReturn.equals("value5"));
+		getReturn = cache.get("key6");
+		assertTrue(getReturn != null);
+		assertTrue(getReturn.equals("value6"));
+		
+		
+		
+		
+		
+		
+	}
 	
 	
 	public void testGetWriteLock() {
