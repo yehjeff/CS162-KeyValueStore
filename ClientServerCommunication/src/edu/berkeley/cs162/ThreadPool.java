fe137db5ea64b9ca8cd40cf9fe3e4fea59058337
Kelvin Chen <kelvin.c.chen@gmail.com>
2013-04-16 17:17:08 -0700

    .start() instead of .run() for threads

--- ClientServerCommunication/src/edu/berkeley/cs162/ThreadPool.java
+++ ClientServerCommunication/src/edu/berkeley/cs162/ThreadPool.java
@@ -60,7 +60,7 @@ public class ThreadPool {
 		threads = new Thread[size];
 		for (int i = 0; i < size; i++){
 			threads[i] = new WorkerThread(this);
-			threads[i].run();							// also temporary
+			threads[i].start();							// also temporary
 		}
 	}
 
