6c3d36fc5c20db86f2303f79e83297d78178b6e1
Kelvin Chen <kelvin.c.chen@gmail.com>
2013-04-17 18:48:10 -0700

    better kvclienttest

--- ClientServerCommunication/src/edu/berkeley/cs162/TestKVClient.java
+++ ClientServerCommunication/src/edu/berkeley/cs162/TestKVClient.java
@@ -3,6 +3,9 @@ package edu.berkeley.cs162;
 import java.io.IOException;
 
 import org.junit.Test;
+import java.util.*;
+
+
 
 import junit.framework.TestCase;
 
@@ -14,7 +17,7 @@ public class TestKVClient extends TestCase {
 	KVServer key_server = new KVServer(100, 10);
 	SocketServer socketserver = new SocketServer("localhost", 8080);
 	NetworkHandler handler = new KVClientHandler(key_server);
-	
+		
 	/*
 	KVClient client = new KVClient(server, port);
 	KVServer kvserver = new KVServer(2, 2);
@@ -23,14 +26,21 @@ public class TestKVClient extends TestCase {
 	KVClientHandler ch = new KVClientHandler(kvserver);
 	*/
 	
-	
-
-	@Test
-	public void testKVClient() {
-		// trivial, done above
+	public class runServer implements Runnable {
+		Thread t;
+		runServer(){
+			t = new Thread(this);
+			t.start();
+		}
+		public void run() {
+			startServer();
+		}
+		
 	}
+
 	
 	public void startServer() {
+		
 		System.out.println("Binding Server:");
 		socketserver.addHandler(handler);
 		try {
@@ -44,52 +54,110 @@ public class TestKVClient extends TestCase {
 	}
 	
 	public void closeServer() {
+		System.out.println("closing server");
 		socketserver.stop();
 	}
-
+	
+	@Test
+	public void testClient() {
+		new runServer();
+		
+		KVClient client = new KVClient(server, port);	
+		try {
+			System.out.println("testing put");
+			client.put("fuzzy", "wuzzy");
+			System.out.println("put ok");
+			
+			System.out.println("putting (fuzzy, wuzzy) (again)");
+			client.put("fuzzy", "wuzzy");
+			System.out.println("ok");
+			
+			System.out.println("putting (key2, value2)");
+			client.put("key2", "value2");
+			System.out.println("ok");
+			
+			System.out.println("getting key=fuzzy");			
+			String value = client.get("fuzzy");					
+			System.out.println("returned: " + value);
+			
+			System.out.println("getting key=key2");			
+			String value2 = client.get("key2");					
+			System.out.println("returned: " + value2);
+			
+			System.out.println("deleting key=key2");			
+			client.del("key2");					
+			System.out.println("ok");
+			
+			System.out.println("deleting key=key2 (again)");			
+			client.del("key2");					
+			System.out.println("ok");
+			
+			closeServer();
+		}
+		catch (KVException e){
+			System.out.println(e.getMsg().getMessage());
+		}
+		
+		
+	}
+	
+	// Below tests work individually, but won't serialize properly. instead combined into singluar test function
+	
+	/**
 	@Test
 	public void testPut() {
+		new runServer();
 		
-		startServer();
-		
-		KVClient client = new KVClient(server, port);
-		
+		KVClient client = new KVClient(server, port);	
 		try {
+			System.out.println("testing put");
 			client.put("fuzzy", "wuzzy");
+			System.out.println("ok");
+			closeServer();
 		}
 		catch (KVException e){
 			System.out.println(e.getMsg().getMessage());
 		}
 	}
 
+	
 	@Test
 	public void testGet() {
-		closeServer();
-		startServer();
+		new runServer();
 		
 		KVClient client = new KVClient(server, port);
 		
 		try {
+			System.out.println("testing get");
 			client.get("fuzzy");
+			System.out.println("ok");
+			closeServer();
+
 		}
 		catch (KVException e){
 			System.out.println(e.getMsg().getMessage());
 		}
 	}
 
+	
+
+	
 	@Test
 	public void testDel() {
-		closeServer();
-		startServer();
+		new runServer();
 		
 		KVClient client = new KVClient(server, port);
 		
 		try {
+			System.out.println("testing del");
 			client.del("fuzzy");
+			System.out.println("ok");
+			//closeServer();
+
 		}
 		catch (KVException e){
 			System.out.println(e.getMsg().getMessage());
 		}
 	}
-
+	*/
 }
