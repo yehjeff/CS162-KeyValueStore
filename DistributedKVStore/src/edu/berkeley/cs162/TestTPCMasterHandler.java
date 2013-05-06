4a7a9e1f9cbfba9cf2fe613fdc7648e573a5e75c
Kelvin Chen <kelvin.c.chen@gmail.com>
2013-05-05 20:31:50 -0700

    (empty) test case for master handler

---
+++ DistributedKVStore/src/edu/berkeley/cs162/TestTPCMasterHandler.java
@@ -0,0 +1,40 @@
+package edu.berkeley.cs162;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTPCMasterHandler{
+	//setting up a server, client, slaves...
+	
+	
+	@Test
+	public void testPut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDel() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPhase1Fail() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPhase2Fail() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testIgnoreNext() {
+		fail("Not yet implemented");
+	}
+}
